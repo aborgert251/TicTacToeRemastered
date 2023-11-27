17c4877e6d9692f73242f0ce6fcd02eb1cd9bddf
André Borgert <95171469+aborgert251@users.noreply.github.com>
2023-11-27 22:50:27 +0100

    docs: add documentation for webcam component

--- src/app/tictactoe/webcam/webcam.component.ts
+++ src/app/tictactoe/webcam/webcam.component.ts
@@ -8,18 +8,35 @@ import { HandTrackingService } from '../services/handtracking.service';
 })
 export class WebcamComponent implements AfterViewInit {
 
+  /**
+   * Get the video HTML Element
+   */
   @ViewChild('video') video?: ElementRef<HTMLVideoElement>;
 
+  /**
+   * Get the canvas HTML Element
+   */
   @ViewChild('canvas') canvas?: ElementRef<HTMLCanvasElement>;
 
+  /**
+   * Inject some dependencies via Dependency Injection
+   * 
+   * @param {HandtrackingService} handtrackingService - The service that handles all hand tracking related stuff
+   */
   constructor(private handtrackingService: HandTrackingService) { }
 
-  ngAfterViewInit(): void {
+  /**
+   * Initialize the handTrackingService by passing the HTML Video and Canvas element into initialize function of the service.
+   */
+  ngAfterViewInit() {
     if(!this.video || !this.canvas) {
       return;
     }
 
-    this.handtrackingService.initialize(this.video.nativeElement, this.canvas.nativeElement);
+    this.handtrackingService.initialize(
+      this.video.nativeElement, 
+      this.canvas.nativeElement
+    );
   }
 
 }
